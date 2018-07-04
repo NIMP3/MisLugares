5689c237e55d371117a1af625810824e59cc7ce7
EDWIN ORBES <yovany.dev@gmail.com>
2018-07-03 22:39:09 -0500

    UI del Formulario para el registro de Lugares terminado

--- app/src/main/java/com/yovanydev/mislugares/FormActivity.java
+++ app/src/main/java/com/yovanydev/mislugares/FormActivity.java
@@ -2,6 +2,7 @@ package com.yovanydev.mislugares;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 
 public class FormActivity extends AppCompatActivity {
 
@@ -9,5 +10,14 @@ public class FormActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_form);
+
+        showToolbar(getResources().getString(R.string.app_name), true);
+    }
+
+    public void showToolbar(String title, Boolean upButton) {
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setTitle(title);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(upButton);
     }
 }
