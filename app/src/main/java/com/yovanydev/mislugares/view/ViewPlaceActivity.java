b23e3ca037c8aa6c88f10e0ba16313261064cea3
EDWIN ORBES <yovany.dev@gmail.com>
2018-09-07 11:35:08 -0500

    Añadiendo fotografías desde la cámara y Cargando fotografías grandes de forma eficiente con BitmapFactory o Picasso

--- app/src/main/java/com/yovanydev/mislugares/view/ViewPlaceActivity.java
+++ app/src/main/java/com/yovanydev/mislugares/view/ViewPlaceActivity.java
@@ -1,8 +1,14 @@
 package com.yovanydev.mislugares.view;
 
+import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.net.Uri;
+import android.os.Environment;
+import android.provider.MediaStore;
+import android.support.design.widget.Snackbar;
 import android.support.v4.app.Fragment;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
@@ -15,14 +21,18 @@ import android.view.View;
 import android.widget.ImageView;
 import android.widget.RatingBar;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.getbase.floatingactionbutton.FloatingActionButton;
 import com.squareup.picasso.Picasso;
+import com.squareup.picasso.Transformation;
 import com.yovanydev.mislugares.R;
 import com.yovanydev.mislugares.model.Place;
 import com.yovanydev.mislugares.model.Places;
 import com.yovanydev.mislugares.utilities.ToolbarUtility;
 
+import java.io.File;
+import java.io.FileNotFoundException;
 import java.text.DateFormat;
 import java.util.Date;
 
@@ -48,6 +58,7 @@ public class ViewPlaceActivity extends AppCompatActivity {
 
     private long id;
     private Place place;
+    private Uri uriPhoto;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -180,8 +191,81 @@ public class ViewPlaceActivity extends AppCompatActivity {
     * @param <code>ImageView imageView</code> : Vista para la fotografia
     * @param <code>String uri</code> : Ruta de la fotografia*/
     protected void putPhoto(ImageView imageView, String uri) {
-        if (uri != null) Picasso.get().load(uri).into(imageView);
-        else Picasso.get().load(R.drawable.place).into(imageView);
+        if (uri != null) {
+            /*Picasso.get()
+                    .load(uri)
+                    .resize(1024,1024)
+                    .error(R.drawable.place)
+                    .centerCrop()
+                    .into(imageView);*/
+            imageViewPicturePlace.setImageBitmap(reduceBitmap(this,uri,1024,1024));
+        }
+        else {
+            Picasso.get()
+                    .load(R.drawable.place)
+                    .into(imageView);
+        }
+    }
+
+    /*----------------------------------------------------------------------------------------------
+    * Toma una fotografia con la camara del celular
+    *
+    * @param <code>View view</code> : Vista que invoca al metodo*/
+    public void takePhoto(View view) {
+        Intent intent = new Intent("android.media.action.IMAGE_CAPTURE");
+        uriPhoto = Uri.fromFile(new File(
+                Environment.getExternalStorageDirectory() + File.separator
+                + "img_" + (System.currentTimeMillis()/1000) + ".jpg"));
+        intent.putExtra(MediaStore.EXTRA_OUTPUT, uriPhoto);
+        startActivityForResult(intent, PHOTO_RESULT);
+    }
+
+    /*----------------------------------------------------------------------------------------------
+    * Reduce el tamaño de una Imagen para optimizar su presentación
+    *
+    * @param context : Contexto en el que se aplican los cambios
+    * @param uri : dirección unica de la foto
+    * @param maxWidth : Ancho maximo para la foto
+    * @param maxHeight : Alto maximo para la foto
+    *
+    * @return <code>Bitmap</code> : Imagen en un Bitmap con factor de reducción*/
+    public static Bitmap reduceBitmap(Context context, String uri, int maxWidth, int maxHeight) {
+        try {
+            final BitmapFactory.Options options = new BitmapFactory.Options();
+            /*Evita decodificar toda la fotografía y cargarla en memoria
+            * Limita a obtener sus dimensiones y almacenarlas en
+            * <code>options.outWidth</code> - <code>options.outHeight</code>*/
+            options.inJustDecodeBounds = true;
+
+            BitmapFactory.decodeStream(
+                    context.getContentResolver().openInputStream(Uri.parse(uri)),
+                    null,
+                    options);
+            /*Calcula el factor de reducción deseado y lo almacena en
+            * <code>options.inSampleSize</code>
+            * Dividimos el ancho y alto real y los dividimos por los respectivos
+            * maxancho y max alto, redondeamos hacia arriba
+            * Seleccionamos el mayor de los dos
+            * BitmapFactory trabaja con factores de reducción que sean potencias de dos
+            *
+            * Example: foto(4096x3072) [factor_reducción(4)] foto_reducción(1024x768)*/
+            options.inSampleSize = (int) Math.max(
+                    Math.ceil(options.outWidth / maxWidth),
+                    Math.ceil(options.outHeight / maxHeight));
+            /*Ahora requerimos decodificar toda la imagen por lo que desactivamos
+            * <code>inJustDecodeBounds</code>*/
+            options.inJustDecodeBounds = false;
+            /*Decodificamos la nueva imagen con la opción <code>inSampleSize</code>
+            * como factor de reducción*/
+            return BitmapFactory.decodeStream(
+                    context.getContentResolver().openInputStream(Uri.parse(uri)),
+                    null,
+                    options);
+        } catch (FileNotFoundException e) {
+            Toast.makeText(context, "Fichero/Recurso no encontrado", Toast.LENGTH_SHORT).show();
+            e.printStackTrace();
+            return null;
+        }
     }
 
     @Override
@@ -219,6 +303,11 @@ public class ViewPlaceActivity extends AppCompatActivity {
         if (requestCode == EDIT_RESULT) updateDateView();
         else if (requestCode == GALLERY_RESULT && resultCode == RESULT_OK) {
             place.setPhotoPlace(data.getDataString());
+            putPhoto(imageViewPicturePlace, place.getPhotoPlace());
+        }
+        else if (requestCode == PHOTO_RESULT && resultCode == RESULT_OK
+                && place != null && uriPhoto != null) {
+            place.setPhotoPlace(uriPhoto.toString());
             putPhoto(imageViewPicturePlace, place.getPhotoPlace());
         }
     }
